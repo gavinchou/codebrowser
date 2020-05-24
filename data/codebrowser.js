80945d0bef6acbe43373cc015c419d3da2211515
Gavin Chou <gavineaglechou@gmail.com>
2020-05-24 22:26:27 +0800

    Add monokai style

--- data/codebrowser.js
+++ data/codebrowser.js
@@ -27,6 +27,7 @@ if (!data_path) {
 //Styles:
 var setStyle = "";
 document.write("<link rel='alternate stylesheet' title='Solarized' href='" + data_path + "/solarized.css' />");
+document.write("<link rel='alternate stylesheet' title='Monokai' href='" + data_path + "/monokai.css' />");
 function switchStylestyle(styleName) {
     setStyle = styleName;
     $('link[rel*=style][title]').each(function(i) {
